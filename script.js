d79832ce5bc9f8c57f2b5f1c19c5ece5db448ea2
AUD-GITHUB <85228103+AUD-GITHUB@users.noreply.github.com>
2023-05-30 14:48:46 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,41 @@
+function preload() {
+}
+
+function setup() {
+    canvas = createCanvas(500, 400);
+    canvas.position(375, 220);
+    video = createCapture(VIDEO);
+    video.hide();
+}
+
+function draw() {
+    image(video, 0, 0, 640, 500);
+    stroke(0, 255, 0);
+    fill(0, 0, 250);
+    rect(0, 0, 500, 15);
+    rect(0, 385, 500, 15);
+    rect(0, 0, 15, 400);
+    rect(485, 0, 15, 400);
+    stroke(0, 0, 255);
+    fill(250, 0, 0);
+    circle(0, 0, 75);
+    circle(500, 0, 75);
+    circle(0, 400, 75);
+    circle(500, 400, 75);
+    stroke(0, 250, 0);
+    fill(0, 250, 0);
+    rect(50, 37.5, 400, 25);
+    rect(50, 400-62.5, 400, 25);
+    rect(37.5, 37.5, 25, 300);
+    rect(500-62.5, 37.5, 25, 300);
+    stroke(0, 0, 255);
+    fill(250, 0, 0);
+    ellipse(50, 50, 35, 35);
+    ellipse(500 - 50, 50, 35, 35);
+    ellipse(50, 400 - 50, 35, 35);
+    ellipse(500 - 50, 400 - 50, 35, 35);
+}
+
+function take_snapshot(){
+    save("Framed Photo.png")
+}
